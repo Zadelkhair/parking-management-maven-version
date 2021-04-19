08361e4fb13c55f32bee3d5a552c6d8beee360d1
zadelkhair <abdo.zad.raja2@gmail.com>
2021-04-19 15:24:28 +0000

    Create reservation template

---
+++ src/main/java/org/example/controller/PositionCardViewController.java
@@ -0,0 +1,48 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PositionCardViewController implements Initializable {
+
+    @FXML
+    private Label lbl_num_place;
+
+    private boolean disponible;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.setDisponible(false);
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+
+        if(disponible){
+            if (this.lbl_num_place.getStyleClass().contains("place-reserver")) {
+                this.lbl_num_place.getStyleClass().remove("place-reserver");
+            }
+            this.lbl_num_place.getStyleClass().add("place-disponible");
+        }
+        else{
+            if (this.lbl_num_place.getStyleClass().contains("place-disponible")) {
+                this.lbl_num_place.getStyleClass().remove("place-disponible");
+            }
+            this.lbl_num_place.getStyleClass().add("place-reserver");
+        }
+
+
+        this.disponible = disponible;
+    }
+
+    public void setNum(int i) {
+        this.lbl_num_place.setText(i+"");
+    }
+}
--- src/main/java/org/example/controller/ReservationStep2ViewController.java
+++ src/main/java/org/example/controller/ReservationStep2ViewController.java
@@ -1,17 +1,27 @@
 package org.example.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import org.example.App;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
+import java.util.Random;
 import java.util.ResourceBundle;
 
-public class ReservationStep2ViewController implements IReceiveData , Initializable {
+public class ReservationStep2ViewController implements IReceiveData, Initializable {
 
     @FXML
     private Label lbl_place;
+
+    @FXML
+    private FlowPane flowpanel;
 
     private int voiture_id = -1;
 
@@ -26,13 +36,33 @@ public class ReservationStep2ViewController implements IReceiveData , Initializa
 
     @Override
     public void setData(Map<String, Object> data) {
-        if(data.containsKey("voiture_id")){
+        if (data.containsKey("voiture_id")) {
             this.setVoiture_id((Integer) data.get("voiture_id"));
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        lbl_place.setText(String.valueOf(this.getVoiture_id()));
+
+        try {
+            for (int i = 0; i < 20; i++) {
+                FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("position_card.fxml"));
+
+                Parent root = fxmlLoader.load();
+
+                PositionCardViewController positionCardViewController = (PositionCardViewController) fxmlLoader.getController();
+                positionCardViewController.setNum(i+1);
+                positionCardViewController.setDisponible((new Random()).nextBoolean());
+
+                flowpanel.getChildren().add(root);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onClickConfermer(MouseEvent mouseEvent) {
+
     }
 }
