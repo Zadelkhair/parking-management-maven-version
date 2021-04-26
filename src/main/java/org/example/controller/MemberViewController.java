edecc342f3fd3db1088bebf35e2e05ce9fe66464
zadelkhair <abdo.zad.raja2@gmail.com>
2021-04-26 12:59:07 +0000

    crud members page

--- src/main/java/org/example/controller/MemberViewController.java
+++ src/main/java/org/example/controller/MemberViewController.java
@@ -47,54 +47,149 @@ public class MemberViewController {
 
     private List<Member> members;
     private List<CmbTypeMember> typeMembers;
+    private Member selectedMember;
 
     @FXML
     void onClickSauvgarder(ActionEvent event) {
+        if(selectedMember!=null){
+            //update
+            selectedMember.setName(txt_nom.getText());
+            selectedMember.setPrenom(txt_prenom.getText());
+            selectedMember.setDebut_abonnement(Date.valueOf(date_d.getValue()));
+            selectedMember.setFin_abonnement(Date.valueOf(date_f.getValue()));
+            CmbTypeMember selectedTypeMember = cmb_type_members.getSelectionModel().getSelectedItem();
 
+            if(selectedTypeMember==null)
+                return;
+
+            selectedMember.setId_member_type(selectedTypeMember.getId());
+
+            if(selectedMember.update()){
+                int i = members.indexOf(selectedMember);
+                if(i>=0){
+                    members.set(i,selectedMember);
+                    System.out.println(typeMembers.get(i).getLebelle());
+                }
+                loadTableviewMembersData();
+                clearInputs();
+            }
+        }
+        else{
+            //create
+            Member member = new Member();
+            member.setId(-1);
+            member.setName(txt_nom.getText());
+            member.setPrenom(txt_prenom.getText());
+            member.setDebut_abonnement(Date.valueOf(date_d.getValue()));
+            member.setFin_abonnement(Date.valueOf(date_f.getValue()));
+            CmbTypeMember selectedTypeMember = cmb_type_members.getSelectionModel().getSelectedItem();
+            member.setId_member_type(selectedTypeMember.getId());
+
+            if(member.create()){
+                members.add(member);
+                loadTableviewMembersData();
+                clearInputs();
+            }
+        }
     }
 
     @FXML
     void onClickSupprimer(ActionEvent event) {
-
+        if(selectedMember!=null){
+            if(selectedMember.delete()){
+                members.remove(selectedMember);
+                loadTableviewMembersData();
+                selectedMember = null;
+                this.clearInputs();
+            }
+        }
     }
 
     @FXML
     void initialize() {
+
+        this.loadMembresData();
+        this.loadTypeMembresData();
+        this.loadTableviewMembersColumns();
+        this.loadTableviewMembersData();
+        this.loadComboboxTypeMemberData();
+
+    }
+
+    public void loadTypeMembresData(){
+        typeMembers = new ArrayList<>();
+
+        List<Map<String,Object>> all = (new CmbTypeMember()).getAll(true);
+
+        for (Map<String,Object> row:all) {
+            CmbTypeMember tm = new CmbTypeMember();
+            tm.readRow(row);
+            typeMembers.add(tm);
+        }
+    }
+
+    public void loadMembresData(){
         members = new ArrayList<>();
 
-        Member m1 = new Member();
-        m1.setId(1);
-        m1.setName("zad");
-        m1.setPrenom("abdelkoddous");
-        m1.setDebut_abonnement(new Date(2020,1,1));
-        m1.setFin_abonnement(new Date(2020,5,5));
-        m1.setId_member_type(1);
-        members.add(m1);
+        List<Map<String,Object>> all = (new Member()).getAll(true);
 
-        tablev_members.setItems(FXCollections.observableList(members));
+        for (Map<String,Object> row:all) {
+            Member m = new Member();
+            m.readRow(row);
+            members.add(m);
+        }
+    }
 
+    public void loadTableviewMembersData(){
+        tablev_members.getColumns().get(0).setVisible(false);
+        tablev_members.getColumns().get(0).setVisible(true);
+        ObservableList<Member> memberObservableList = FXCollections.observableList(members);
+        tablev_members.setItems(memberObservableList);
+    }
+
+    public void loadComboboxTypeMemberData(){
+        //load cmb type members
+        ObservableList<CmbTypeMember> typeMemberObservableList = FXCollections.observableList(typeMembers);
+        cmb_type_members.setItems(typeMemberObservableList);
+    }
+
+    public void loadTableviewMembersColumns(){
+        TableColumn column = new TableColumn("id");
+        column.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tablev_members.getColumns().add(column);
         for (Map.Entry<String, Object> entry : (new Member()).toRow().entrySet()) {
-            TableColumn column = new TableColumn(entry.getKey());
+            column = new TableColumn(entry.getKey());
             column.setCellValueFactory(new PropertyValueFactory<>(entry.getKey()));
             tablev_members.getColumns().add(column);
         }
-
-        //load cmb type members
-        typeMembers = new ArrayList<>();
-        CmbTypeMember tm1 = new CmbTypeMember();
-        tm1.setId(1);
-        tm1.setLebelle("Hot");
-        typeMembers.add(tm1);
-        cmb_type_members.setItems(FXCollections.observableList(typeMembers));
-
     }
 
     public void onMouseClickTableView(MouseEvent mouseEvent) {
         Object obj = tablev_members.getSelectionModel().getSelectedItem();
         if (mouseEvent.getClickCount() == 2 && obj != null) {
-            System.out.println(members.indexOf(obj));
-            System.out.println(typeMembers.indexOf(cmb_type_members.getValue()));
+            if(members.indexOf(obj) >= 0){
+                selectedMember = (Member)obj;
+                txt_nom.setText(selectedMember.getName());
+                txt_prenom.setText(selectedMember.getPrenom());
+                date_d.setValue(selectedMember.getDebut_abonnement().toLocalDate());
+                date_f.setValue(selectedMember.getFin_abonnement().toLocalDate());
+
+                CmbTypeMember typeMember = new CmbTypeMember();
+                typeMember.setId(selectedMember.getId_member_type());
+                if(typeMember.read()){
+                    cmb_type_members.getSelectionModel().select(typeMember);
+                }
+
+            }
         }
+    }
+
+    private void clearInputs() {
+        txt_prenom.clear();
+        txt_nom.clear();
+        cmb_type_members.getSelectionModel().clearSelection();
+        date_d.setValue(null);
+        date_f.setValue(null);
     }
 
     class CmbTypeMember extends TypeMember {
@@ -103,4 +198,5 @@ public class MemberViewController {
             return this.getLebelle();
         }
     }
+
 }
