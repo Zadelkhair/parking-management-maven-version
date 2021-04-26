22b8df851d9d5ef72b5ffe906a9a6cdc3acd339d
DohaM9 <dohamido98@gmail.com>
2021-04-27 00:50:48 +0200

    adding just some error message and confirmation message

--- src/main/java/org/example/controller/VoitureViewController.java
+++ src/main/java/org/example/controller/VoitureViewController.java
@@ -1,4 +1,47 @@
 package org.example.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class VoitureViewController {
+    @FXML
+    private TextField txt_matricule;
+
+    @FXML
+    private TextField txt_Couleur;
+
+    @FXML
+    private TextField txt_Marque;
+
+    @FXML
+    private ComboBox<?> cmb_type_voiture;
+
+    @FXML
+    private Button Supprimer;
+
+    @FXML
+    private Button Sauvegarder;
+
+    @FXML
+    private TableView<?> tablev_members;
+
+    @FXML
+    void onClickSauvgarder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSupprimer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickTableView(MouseEvent event) {
+
+    }
 }
