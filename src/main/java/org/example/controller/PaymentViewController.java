9eacf7819c97a112b750a1a2847b65722a5925ef
ayachhamza <hamza.ayach.98@gmail.com>
2021-04-27 16:50:48 +0000

    creating payment view and controller

---
+++ src/main/java/org/example/controller/PaymentViewController.java
@@ -0,0 +1,40 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class PaymentViewController {
+
+    @FXML
+    private TextField txt_name;
+
+    @FXML
+    private TextField txt_mat;
+
+    @FXML
+    private DatePicker dp_entree;
+
+    @FXML
+    private DatePicker dp_sortie;
+
+    @FXML
+    private TextField txt_prix;
+
+    @FXML
+    private TextField txt_duree;
+
+    @FXML
+    private TextField txt_no_place;
+
+    @FXML
+    void onClickSauvgarder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSupprimer(ActionEvent event) {
+
+    }
+
+}
