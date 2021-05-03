a547140e5ecfa2f29b37cd942d006683b5753609
DohaM9 <dohamido98@gmail.com>
2021-05-03 12:36:03 +0200

    page payment

---
+++ src/main/java/org/example/controller/Payment.java
@@ -0,0 +1,46 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Payment {
+    @FXML
+    private TextField txt_nomprenom;
+
+    @FXML
+    private TextField txt_typem;
+
+    @FXML
+    private TextField txt_matricule;
+
+    @FXML
+    private TextField txt_prix;
+
+    @FXML
+    private TextField txt_num_place;
+
+    @FXML
+    private TextField txt_datee;
+
+    @FXML
+    private TextField txt_dates;
+
+    @FXML
+    private Button Submit;
+
+    @FXML
+    private Button Annuler;
+
+    @FXML
+    void onClickAnnuler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSubmit(ActionEvent event) {
+
+    }
+
+}
