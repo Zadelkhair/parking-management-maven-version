de628b6768f038fe37ff2d12ea95abee3d048f57
ZADELKHAIR Abdelkoddous <35137927+Zadelkhair@users.noreply.github.com>
2021-05-08 00:44:21 +0000

    Update IReceiveData.java

--- src/main/java/org/example/controller/IReceiveData.java
+++ src/main/java/org/example/controller/IReceiveData.java
@@ -2,6 +2,9 @@ package org.example.controller;
 
 import java.util.Map;
 
+/*
+* Simple but powerfull , make controller's exchange data's
+*/
 public interface IReceiveData {
 
     void setData(Map<String, Object> data);
